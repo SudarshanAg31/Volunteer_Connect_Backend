1ee655dea1984cc846d5d9573f68e7574415343b
SudarshanAg31 <sudarshanagrawal65@gmail.com>
2026-09-04 19:49:46 +0530

    update ALLOWED_HOSTS to include the correct production domain

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-wnj3apde7qz+4nuu&9hf49)(h_fkt(7rcognzw*m*h9h%(t()u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://volunteer-connect-backend-yy38.onrender.com"]
+ALLOWED_HOSTS = ["SudarshanAg31.pythonanywhere.com"]
 
 
 # Application definition
